83487b97bf3fd36688a84f86205fc7515df3f20d
AshaF4 <asha.fidow@kpmg.co.uk>
2022-07-26 09:41:10 +0100

    changes in html and css

--- src/App.js
+++ src/App.js
@@ -6,17 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload. See what happens! Some extra change too
+          This is a CI/CD exercise.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
+        
       </header>
     </div>
   );
